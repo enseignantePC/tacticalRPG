9c9e87403f3554d87caf3b55b213995eb6a04bcc
Astrale <>
2020-05-03 16:51:19 +0200

    ça m'a soulé

--- src/Skeletons/FightSystem/Objects/FightObject/dependancy/Team/dependancy/FightActor/Move.gd
+++ src/Skeletons/FightSystem/Objects/FightObject/dependancy/Team/dependancy/FightActor/Move.gd
@@ -1,21 +1,42 @@
 extends Option
 
+signal Moved(from,to)
+
+var path_move_points = "Status/MovePoints"
+var path_map_pos = "Status/map_pos"
+var path_Terrains = "Characteristics/Terrains"
+
+var map : MapIntern
+var pos : Position2D
+var max_cost : float
+var terrains : Dictionary
+var _owner : FightActor
+var team_id : int
+
+func get_values(_FightContext):
+	_owner  = owner
+	team_id = _owner.team_id
+	map = _FightContext.get_map()
+	pos = _owner.get_node(path_map_pos)
+	max_cost = _owner.get_node(path_move_points).value
+	terrains = _owner.get_node(path_Terrains).value	
 
 func get_possible_places(_FightContext : FightContext)->Array:
 	var possibles
-	var _owner : FightActor = owner
-	var team_id = _owner.team_id
-	var map : MapIntern = _FightContext.get_map()
-	var pos : Position2D = _owner.get_node("Status/map_pos")
-	var max_cost = _owner.get_node("Status/MovePoints").value
-	var terrains = _owner.get_node("Characteristics/Terrains").value
+	get_values(_FightContext)
 	return map.get_possibles(
 			team_id,
 			pos.position,
 			terrains,
 			max_cost)
 	
-func move_to_pos():
+func move_to_pos(to_pos,_FightContext : FightContext):
 	#inform
 	#reduce move points
-	pass
+	get_values(_FightContext)
+	if not to_pos in get_possible_places(_FightContext):
+		return printerr("posiiton non reachable")
+
+	var cost = map.get_cost_to(team_id, pos.position,to_pos, max_cost,terrains)
+	emit_signal("Moved",pos,to_pos)
+	
--- src/Skeletons/services/calculation services/MapIntern/map_intern.gd
+++ src/Skeletons/services/calculation services/MapIntern/map_intern.gd
@@ -77,7 +77,6 @@ func move_obstacle_case_from_to(from,to):
 	if case_is_busy(to): return FAILED
 	ObstacleLayer[to] = ObstacleLayer[from]
 	ObstacleLayer[from] = EMPTY
-	
 	return OK
 	
 func free_obstacle_case(team_id,pos):
@@ -91,9 +90,8 @@ func free_body_case(team_id,pos):
 func get_possibles(team_id,map_pos,terrains = {},max_cost = INF):
 	"returns all reachable positions from map_pos for the member of team_id, map_pos excluded"
 	var possibles := []
-	
 	map.recalculate(
-		map.position_to_id(map_pos),
+		PoolIntArray(map.position_to_id(map_pos)),
 		{
 		'input is destination' : false,
 		'maximum cost' : max_cost,
@@ -101,3 +99,14 @@ func get_possibles(team_id,map_pos,terrains = {},max_cost = INF):
 		'terrain weights' : terrains,
 		}
 	)
+func get_cost_to(team_id, from : Vector2,to : Vector2, max_cost = INF,terrains={}):
+#	var BodiesLayer = Team_id_to_BodiesLayer[team_id]
+	map.recalculate(
+					PoolIntArray(map.position_to_id(from)),
+					{'input is destination' : false,
+					'maximum cost' : max_cost,
+					'initial costs' : 0.0,
+					'terrain weights' : terrains
+					}
+					)
+	map.NativeMap.get_cost_at_point()
