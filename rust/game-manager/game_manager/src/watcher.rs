8d754c75e9a85eff637088689e5f4b4a170b07c1
Astrale <jules180798@yahoo.fr>
2021-09-03 22:23:14 +0200

    adds a todo since the watch method of the Watcher struct doesnt do its job yet

--- rust/game-manager/game_manager/src/watcher.rs
+++ rust/game-manager/game_manager/src/watcher.rs
@@ -6,6 +6,7 @@ impl Watcher {
     /// be capable of reacting (submitting an intent) that depends on the intent watched
     #[deprecated]
     pub fn watch(&mut self, next_intent: &Intent) -> Vec<Intent> {
+        todo!();
         Vec::new()
     }
 }
