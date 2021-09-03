edd1e5b4997f2ea228e93ec356af8f433798c680
Astrale <jules180798@yahoo.fr>
2021-09-03 20:50:56 +0200

    some more work on attack solving, what contet will it needs, what details will it provide

--- rust/game-manager/game_manager/src/attack_solver.rs
+++ rust/game-manager/game_manager/src/attack_solver.rs
@@ -1,8 +1,14 @@
 use super::*;
+
+/// TODO should accept more context :
+/// -from what terrains to what terrains does the entity attack
+/// - is it raining or whatever
+/// what kind of curse (tags?) are on the map for both people
 pub fn solve(attack: Attack, entity_assailant: &Entity, entity_attacked: &Entity) -> AttackResult {
     let damage: f64 = attack.strength * entity_attacked.entity_intern.damage_reduction_factor();
     AttackResult {
         damage_dealed: damage,
+        critical_hit: false,
     }
 }
 
