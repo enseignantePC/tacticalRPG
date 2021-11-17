40a68893ed1b13592f4545cb43faeb32e8b952bb
Astrale <jules180798@yahoo.fr>
2021-11-17 15:55:11 +0100

    Adds some new integration tests that will pass later

--- rust/game-manager/game_manager/tests/integration.rs
+++ rust/game-manager/game_manager/tests/integration.rs
@@ -1,11 +1,27 @@
 use game_manager;
 
+fn new_manager() {
+    let mut builder = todo!();
+    let id_dirt = builder.with_terrain_crossable("Dirt");
+    let id_water = builder.with_terrain_attack_crossable("Water");
+    let id_wall = builder.with_terrain_crossable("Wall");
+    builder.with_map(todo!());
+    builder.build()
+}
+
+#[test]
+fn build_manager() {
+    new_manager()
+}
+
 #[test]
 fn simple_moving_around() {
+    let gm = new_manager();
     todo!()
 }
 
 #[test]
 fn impl_counter_attacks() {
+    let gm = new_manager();
     todo!()
 }
